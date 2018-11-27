aa8e4d35ea8be0adb55be0db27895296db0a5a62
akshata <wagheakshu@gmail.com>
2018-11-27 13:31:11 +0530

    create demo file for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Items from './Items';
+import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders correctly when there are no items', () => {
+  const tree = renderer.create(<Items />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there are only one item', () => {
+  const items = ['one'];
+  const tree = renderer.create(<Items items={items} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it.skip('renders correctly when there are multiple items', () => {
+  const items = ['one', 'two', 'three'];
+  const tree = renderer.create(<Items />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
